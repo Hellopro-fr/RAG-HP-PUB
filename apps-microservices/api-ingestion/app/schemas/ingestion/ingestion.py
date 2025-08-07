95c1370c55cc43f1e9e352b2cd1aa6c76d3c3558
sandrianirinaharivelo <sandrianirinaharivelo@hellopro.fr>
2025-08-07 10:17:03 +0300

    feat(maj): modification données en entrée ingestion

--- apps-microservices/api-ingestion/app/schemas/ingestion/ingestion.py
+++ apps-microservices/api-ingestion/app/schemas/ingestion/ingestion.py
@@ -3,9 +3,13 @@ import uuid
 from pydantic import ConfigDict, BaseModel, Field
 from app.schemas.base import GetBase as Base
 
-
+class DataIngestion(BaseModel):
+    info: Annotated[dict, Field(title="Les données à publier", description="Un objet JSON contenant les informations du produit ou autre.")]
+    collection: Annotated[str, Field(title="La collection pour insertion dans Milvus")]
+    embedding: Annotated[str, Field(title="Texte à vectoriser dans milvus")] = ""
+    
 class BaseIngestion(BaseModel):
-    data: Annotated[dict, Field(title="Les données à publier", description="Un objet JSON contenant les informations du produit ou autre.")]
+    data: Annotated[DataIngestion, Field(title="Les données à publier", description="Un objet JSON contenant les informations du produit ou autre.")]
     exchange_name: Annotated[str, Field(title="Nom de l'exchange RabbitMQ")] = "data_exchange"
     routing_key: Annotated[str, Field(title="Clé de routage pour le message")]
 
