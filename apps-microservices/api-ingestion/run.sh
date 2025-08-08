96efa4ec376d7677798ef0f777987b1aa902f705
sandrianirinaharivelo <sandrianirinaharivelo@hellopro.fr>
2025-08-08 22:09:31 +0300

    feat(maj): suppression debug deploiement render

--- apps-microservices/api-ingestion/run.sh
+++ apps-microservices/api-ingestion/run.sh
@@ -2,13 +2,5 @@
 
 source .venv/bin/activate
 # pip install -r requirements.txt
-ls -la
-cd ./app/
-ls -la
-cd ./common_utils/
-ls -la
-cd ./src/
-ls -la
-cd ../../..
 
 uvicorn main:app --host 0.0.0.0 --port 8509 --reload
