16faec14a827337e507811461334a4f0c2b60346
sandrianirinaharivelo <sandrianirinaharivelo@hellopro.fr>
2025-08-08 21:33:53 +0300

    feat(maj): modification require collectionname

--- apps-microservices/api-ingestion/app/core/ingestion/ingestion.py
+++ apps-microservices/api-ingestion/app/core/ingestion/ingestion.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from common_utils.autres.CollectionName import CollectionName, RoutingKeys as collections
+from libs.common_utils.autres.CollectionName import CollectionName, RoutingKeys as collections
 
 
 def routing_key_collection(collection: CollectionName):
