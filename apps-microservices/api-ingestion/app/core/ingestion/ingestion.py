691f28fe2e89d9ff1da5959f80e52aa3e263d278
unknown <tolojanaharysoary@gmail.com>
2025-08-19 15:54:16 +0300

    Feat(maj): Ajout api-ingestion-service fonctionnel

---
+++ apps-microservices/api-ingestion/app/core/ingestion/ingestion.py
@@ -0,0 +1,9 @@
+from enum import Enum
+from app.common_utils.src.common_utils.autres.CollectionName import CollectionName, RoutingKeys as collections
+
+
+def routing_key_collection(collection: CollectionName):
+    # Use .get() to provide a default value if the key is not found
+    return collections.get(collection, "")
+
+
