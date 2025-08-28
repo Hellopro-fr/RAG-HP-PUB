6e06b03021edcadfef4ab58dfe5414f463582ccd
martial <iandriahertse@hellopro.fr>
2025-08-28 12:55:57 +0300

    Feat(maj): maj url webhook

--- libs/common-utils/src/common_utils/autres/CollectionWebhook.py
+++ libs/common-utils/src/common_utils/autres/CollectionWebhook.py
@@ -6,8 +6,8 @@ from common_utils.autres.CollectionName import CollectionName
 CollectionWebhook = {
     CollectionName.PRODUIT    : "https://webhook.site/44bd6be7-e2b2-42eb-bd04-7b5d27761645",
     CollectionName.DEVIS      : "https://webhook.site/44bd6be7-e2b2-42eb-bd04-7b5d27761645",
-    CollectionName.CATEGORIE  : "https://webhook.site/44bd6be7-e2b2-42eb-bd04-7b5d27761645",
+    CollectionName.CATEGORIE  : "https://webhook.site/c9e32e3d-348e-4df4-9584-f0848d23900b",
     CollectionName.ECHANGE    : "https://webhook.site/44bd6be7-e2b2-42eb-bd04-7b5d27761645",
-    CollectionName.FOURNISSEUR: "https://webhook.site/44bd6be7-e2b2-42eb-bd04-7b5d27761645",
+    CollectionName.FOURNISSEUR: "https://webhook.site/c9e32e3d-348e-4df4-9584-f0848d23900b",
     CollectionName.SITEWEB    : "https://webhook.site/44bd6be7-e2b2-42eb-bd04-7b5d27761645",
   }
