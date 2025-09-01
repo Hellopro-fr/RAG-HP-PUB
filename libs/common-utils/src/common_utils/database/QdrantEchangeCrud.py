e14c25b153f1a0ba1e36159c1cb55d2cb3665b27
unknown <tolojanaharysoary@gmail.com>
2025-09-01 18:10:48 +0300

    Feat(maj): changement config qdrant/milvus 64/400

--- libs/common-utils/src/common_utils/database/QdrantEchangeCrud.py
+++ libs/common-utils/src/common_utils/database/QdrantEchangeCrud.py
@@ -21,7 +21,7 @@ from qdrant_client.http.models import (
 @dataclass
 class ModelConfig:
     model_id: str = settings.MODEL
-    collection_name: str = "echanges_poc_48_500"
+    collection_name: str = "echanges_poc_64_400"
     dimension: int = 1024
 
 
@@ -62,8 +62,8 @@ class QdrantEchangeCrud:
                 collection_name=collection_name,
                 vectors_config=VectorParams(size=model_config.dimension, distance=Distance.COSINE),
 				hnsw_config=HnswConfigDiff(
-					m=48,
-					ef_construct=500
+					m=64,
+					ef_construct=400
 				),
                 shard_number=2,
                 replication_factor=2
--- libs/common-utils/src/common_utils/database/QdrantWebsiteCrud.py
+++ libs/common-utils/src/common_utils/database/QdrantWebsiteCrud.py
@@ -21,7 +21,7 @@ from qdrant_client.http.models import (
 @dataclass
 class ModelConfig:
     model_id: str = settings.MODEL
-    collection_name: str = "siteweb_poc_48_500"
+    collection_name: str = "siteweb_poc_64_400"
     dimension: int = 1024
 
 
@@ -61,8 +61,8 @@ class QdrantWebsiteCrud:
                 collection_name=collection_name,
                 vectors_config=VectorParams(size=model_config.dimension, distance=Distance.COSINE),
 				hnsw_config=HnswConfigDiff(
-					m=48,
-					ef_construct=500
+					m=64,
+					ef_construct=400
 				),
                 shard_number=2,
                 replication_factor=2
