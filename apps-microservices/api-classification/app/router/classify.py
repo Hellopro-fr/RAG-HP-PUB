e5a04100ff0141e49100141dfe459025179ba976
unknown <tolojanaharysoary@gmail.com>
2025-09-02 17:37:42 +0300

    Feat: Ajout structure api-classification

---
+++ apps-microservices/api-classification/app/router/classify.py
@@ -0,0 +1,22 @@
+from fastapi import APIRouter, HTTPException, Body
+from app.schemas.classify import ClassificationRequest, ClassificationReponse
+# from app.core.classify import classify 
+import logging
+
+router = APIRouter()
+logger = logging.getLogger(__name__)
+
+@router.post("/classify", response_model=ClassificationReponse, tags=["Classification"])
+async def classify(request: ClassificationRequest = Body(...)):
+    try:
+        if not request.data.strip():
+            raise ValueError("Le paramètre data ne peut pas être vide.")
+        
+        return ClassificationReponse(response="OK")
+        
+    except ValueError as ve:
+        logger.error(f"Erreur de validation (400): {ve}")
+        raise HTTPException(status_code=400, detail=str(ve))
+    except Exception as e:
+        logger.error(f"Erreur interne du serveur (500): {e}", exc_info=True)
+        raise HTTPException(status_code=500, detail=f"Erreur interne du serveur: {e}")
