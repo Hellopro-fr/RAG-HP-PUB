adee82c35a18886b3707946da01fb8cc37619c9a
TsihoaranaHT <tandriatsiferantsoa@hellopro.fr>
2025-09-03 12:33:36 +0300

    Feat(maj) : Inserer dans une nouvelle base les id des produits vectorisés

---
+++ libs/common-utils/src/common_utils/database/MilvusProduitInsereCrud.py
@@ -0,0 +1,136 @@
+import logging
+from typing import List, Dict, Any, Optional
+from dataclasses import dataclass
+from datetime import datetime
+
+from common_utils.database.config.settings import Configuration, settings
+from common_utils.database.Utils import Utils
+
+from pymilvus import (
+    connections,
+    utility,
+    FieldSchema,
+    CollectionSchema,
+    DataType,
+    Collection,
+    MilvusException
+)
+
+
+@dataclass
+class ModelConfig:
+    model_id: str = settings.MODEL
+    collection_name: str = "produits_insere"
+    dimension: int = 1024
+
+class MilvusProduitsInsereCrud:
+    def __init__(self, config: Configuration = settings , **kwargs: Any):
+        self.config = config
+        self.collection: Optional[Collection] = None
+        # if not self.config.ZILLIZ_URI or not self.config.ZILLIZ_API_KEY:
+        if not self.config.ZILLIZ_URI or not self.config.ZILLIZ_PORT:
+            raise ValueError("Zilliz Cloud URI and API Key/Port must be set in the environment.")
+        self.logger = kwargs.get('logger', logging)
+        
+    def _connect_to_milvus(self):
+        self.logger.info("Connexion sur Zilliz cloud...")
+        # connections.connect("default", uri=self.config.ZILLIZ_URI, token=self.config.ZILLIZ_API_KEY)
+        connections.connect("default", host=self.config.ZILLIZ_URI, port=self.config.ZILLIZ_PORT)
+        self.logger.info("✓ Connexion sur Zilliz cloud avec succès.")
+    
+    # TODO : modification pour les autres collections
+    def _get_or_create_collection(self, model_config: ModelConfig) -> Collection:
+        collection_name = model_config.collection_name
+        model_key = model_config.model_id
+
+        if utility.has_collection(collection_name) and self.config.RECREATE_COLLECTIONS:
+            logging.warning(f"[{model_key}] Collection déjà existante → suppréssion en cours : '{collection_name}'")
+            utility.drop_collection(collection_name)
+
+        if not utility.has_collection(collection_name):
+            self.logger.info(f"Collection '{collection_name}' non trouvée. Création...")
+            # Définition du schéma détaillé
+            fields = [
+                #TODO a completer / verifier
+                FieldSchema(name="id", dtype=DataType.INT64 , is_primary = True , auto_id = True ,max_length=64),
+                FieldSchema(name="id_produit", dtype=DataType.VARCHAR , max_length=64),
+                FieldSchema(name="embedding", dtype=DataType.FLOAT_VECTOR, dim=model_config.dimension),
+                FieldSchema(name="id_milvus_produit", dtype=DataType.VARCHAR, max_length=512),
+
+            ]
+            schema = CollectionSchema(fields, description=f"Collection de chunks de Produit pour {model_key}")
+            
+            collection = Collection(
+                collection_name, 
+                schema,
+                consistency_level="Strong"
+            )
+            
+            self.logger.info(f"[{model_key}] Création HNSW index pour l'embedding")
+
+            # TODO : Vérifier les paramètres d'indexation
+            # Exemple d'indexation HNSW pour les embeddings
+            index_params = {"metric_type": "COSINE", "index_type": "HNSW", "params": {"M": 32, "efConstruction": 200}}
+            collection.create_index(field_name="embedding", index_params=index_params)
+
+            # Optionnel: Créer des index scalaires pour les filtres fréquents
+            collection.create_index(field_name="id_produit", index_name="idx_produit")
+
+            self.logger.info(f"[{model_key}] ✓ Index créés.")
+        else:
+            self.logger.info(f"[{model_key}] Connexion à la collection existante : '{collection_name}'")
+            collection = Collection(collection_name)
+        
+        collection.load()
+        self.logger.info(f"[{model_key}] ✓ Collection '{collection_name}' chargée et prête.")
+        return collection
+
+
+    def insert_produits_insere(self, datas: Dict[str, Any]) -> Dict[str, Any]:
+        model_config = ModelConfig()
+        model_key = model_config.model_id
+
+        try:
+            
+            self._connect_to_milvus()
+            self.collection = self._get_or_create_collection(model_config)
+            
+            if not datas or self.collection is None:
+                return {
+                    "status": "error",
+                    "message": "Aucune donnée à insérer ou collection non initialisée."
+                }
+            
+            self.logger.info(f"[{model_key}][Produits] Insertion de batch de {len(datas)} entités dans '{self.collection.name}'...")
+           
+            sanitized_batch = []
+            for data in datas:
+                data_insert = []
+                data_insert["id_produit"] = data.get("id_produit")
+                data_insert["id_produit_milvus"] = ",".join(data["data"]["ids"])
+                data_insert["embedding"] = [0.1, 0.2, 0.3, 0.4]
+
+                # Sanitize the record to ensure no None values
+                # This is important for Milvus compatibility
+                data_insert = Utils.sanitize_record(data_insert)  
+                sanitized_batch.append(data_insert)
+
+            result = self.collection.insert(sanitized_batch)
+            # self.collection.flush()
+
+            self.logger.info(f"Résultat insertion : {result}") 
+            self.logger.info(f"Clé primaire : {result.primary_keys}") 
+            
+            self.logger.info(f"[{model_key}] ✓ Insertion terminée avec succès.")
+            
+            return {
+                "ids": str(result.primary_keys[0]) if result.primary_keys else "",
+                "status": "success",
+            }
+
+        except MilvusException as e:
+            self.logger.error(f"[{model_key}][Produits] Erreur Milvus lors de l'insertion : {e}")
+            self.logger.error(f"Data : {datas}")
+        except Exception as e:
+            self.logger.error(f"[{model_key}][Produits] insertion de batch : {e}", exc_info=True)
+            self.logger.error(f"Data : {datas}")
