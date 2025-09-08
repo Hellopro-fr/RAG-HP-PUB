326017846cc98d68cfe56ab8be6a816556b8e0b4
martial <iandriahertse@hellopro.fr>
2025-09-08 15:07:11 +0300

    fix : load config api check doublon

--- apps-microservices/api-check-doublon-produit/app/core/credentials.py
+++ apps-microservices/api-check-doublon-produit/app/core/credentials.py
@@ -6,20 +6,20 @@ class Settings(BaseSettings):
     # PROJECT_NAME: str = "API-HP-RAG"
     # PROJECT_VERSION: str = "0.0.1"
 
-    # QDRANT_URL: str = ""
-    # QDRANT_HOST_URL: str
-    # QDRANT_PORT: str
-    # QDRANT_API_KEY: str
-    # ZILLIZ_URI: str
-    # ZILLIZ_PORT: str
-    # ZILLIZ_API_KEY: str
-    # RABBITMQ_URL: str
-    # KEY_WEBHOOK: str
-    # OPENAI_API_KEY: str
-    # OPENROUTER_API_KEY: str
-    # DEEPSEEK_API_KEY: str
-    # M_PARAMS: str
-    # EF_PARAMS: str
+    QDRANT_URL: str = ""
+    QDRANT_HOST_URL: str
+    QDRANT_PORT: str
+    QDRANT_API_KEY: str
+    ZILLIZ_URI: str
+    ZILLIZ_PORT: str
+    ZILLIZ_API_KEY: str
+    RABBITMQ_URL: str
+    KEY_WEBHOOK: str
+    OPENAI_API_KEY: str
+    OPENROUTER_API_KEY: str
+    DEEPSEEK_API_KEY: str
+    M_PARAMS: str
+    EF_PARAMS: str
 
     # MILVUS_OUTPUT_FIELDS_CONFIG: Dict[str, List[str]] = {
     #     "devis_poc": [
