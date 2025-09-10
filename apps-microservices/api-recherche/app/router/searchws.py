195ded811f1ea9eb4ed80bcb42a95a911f68b123
sandrianirinaharivelo <sandrianirinaharivelo@hellopro.fr>
2025-09-10 10:18:58 +0300

    feat(maj): ajout connexion manager

--- apps-microservices/api-recherche/app/router/searchws.py
+++ apps-microservices/api-recherche/app/router/searchws.py
@@ -5,6 +5,7 @@ import logging
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect
 from app.core.searchws import search_in_milvus_stream  # We will create this new streaming function
 from app.schemas.search import SearchRequestWs as SearchRequest
+from app.core.ConnexionManager import manager
 
 router = APIRouter()
 logger = logging.getLogger(__name__)
@@ -17,7 +18,8 @@ async def websocket_search(websocket: WebSocket):
     - Waits for a JSON message containing the search request.
     - Calls the streaming search function and sends updates to the client.
     """
-    await websocket.accept()
+    await manager.connect(websocket)
+    # await websocket.accept()
     logger.info("WebSocket connection accepted.")
     try:
         # Wait for the client to send the search request
@@ -32,11 +34,13 @@ async def websocket_search(websocket: WebSocket):
             await websocket.send_json(update)
 
     except WebSocketDisconnect:
-        logger.warning("Client disconnected.")
+        logger.warning("Client {websocket.client} disconnected.")
     except Exception as e:
-        logger.error(f"An error occurred in the WebSocket: {e}", exc_info=True)
+        logger.error(f"An error occurred in the WebSocket pour {websocket.client}: {e}", exc_info=True)
         # Send an error message to the client before closing
-        await websocket.send_json({"type": "error", "payload": str(e)})
+        error_payload  = {"type": "error", "payload": str(e)}
+        await manager.send_personal_message(error_payload, websocket)
     finally:
-        logger.info("Closing WebSocket connection.")
-        await websocket.close()
+        logger.info("Closing WebSocket connection pour {websocket.client}.")
+        manager.disconnect(websocket)
+        # await websocket.close()
