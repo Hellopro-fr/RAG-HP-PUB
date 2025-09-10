3a83f01c6b7c7e86f11c2cddd971cb406fb2a1f5
erick-daniel-ht <erandrianolalaina@hellopro.fr>
2025-09-10 09:05:20 +0300

    feat(maj) modification prompt format JSON

--- apps-microservices/optimize-service/app/core/optimize/Qwen3_14B_AWQ_par_lots.py
+++ apps-microservices/optimize-service/app/core/optimize/Qwen3_14B_AWQ_par_lots.py
@@ -93,12 +93,15 @@ class ProductTitleOptimizerBatch:
             "Titre": "Ton titre généré ici"
             }}}}
 
+            # FORMAT DE SORTIE TRES IMPORTANT : 
+            Ta réponse doit être un JSON strictement valide, sans aucun texte avant ou après, sans balises Markdown, sans ```json, sans commentaire, sans indentation inutile. Exemple :
+            {{"Titre": "Titre généré ici"}}
+
             # INPUTS :
             CATEGORIE PRODUIT = {data.get('categorie_produit', '')}
             TITRE PRODUIT = {data.get('nom_produit', '')}
             DESCRIPTION PRODUIT = {data.get('description_produit', '')}
-
-            Réponse JSON uniquement:"""
+            """
         return prompt
 
     def escape_for_prompt(self, text: str) -> str:
