3da2d8f07358cbfe5474e7865edaf1bbd38a3b80
martial <iandriahertse@hellopro.fr>
2025-09-15 10:47:34 +0300

    Feat(maj) : mise à jour seuil vérification doublon produit

--- apps-microservices/api-check-doublon-produit/app/core/credentials.py
+++ apps-microservices/api-check-doublon-produit/app/core/credentials.py
@@ -21,7 +21,7 @@ class Settings(BaseSettings):
     M_PARAMS: str
     EF_PARAMS: str
     
-    SEUIL_SCORE_DOUBLON      : float = 0.75
+    SEUIL_SCORE_DOUBLON      : float = 0.90
     ADRESSE_VM_API_RECHERCHE : str   = "http://34.90.162.9"
     PORT_API_RECHERCHE       : int   = 8500
     COLLECTION_PRODUIT_NAME  : str   = "produits_3"
