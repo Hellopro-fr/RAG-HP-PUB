fe7096b93731f10e99826c315ca0f5d27207e138
sandrianirinaharivelo <sandrianirinaharivelo@hellopro.fr>
2025-09-15 12:47:18 +0300

    fix: erreur import search_in_qdrant

--- apps-microservices/api-recherche/app/router/search.py
+++ apps-microservices/api-recherche/app/router/search.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException, Body
 from app.schemas.search import SearchRequestWs as SearchRequest, SearchResponse, SearchReponse
-from app.core.search import search_in_qdrant, search_in_milvus
+from app.core.search import search_in_milvus
 import logging
 
 router = APIRouter()
