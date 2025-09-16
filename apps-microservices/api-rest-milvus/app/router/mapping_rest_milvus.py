0e806852185fc64cb4b6c429a75d93424f46f25a
Tanjona Ralita <tanjonaravelonapgmail.com>
2025-09-16 18:10:05 +0300

    feat(maj) : enlever trace debug

---
+++ apps-microservices/api-rest-milvus/app/router/mapping_rest_milvus.py
@@ -0,0 +1,20 @@
+# common_utils/database/milvus_router_registry.py
+
+from common_utils.database import (
+    MilvusProduitCrud,
+    MilvusFournisseursCrud,
+    MilvusDevisCrud,
+    MilvusWebsiteCrud,
+    MilvusCategorieCrud,
+    MilvusEchangeCrud
+)
+
+# Dictionnaire centralisé pour toutes les opérations CRUD
+MILVUS_CRUD_REGISTRY = {
+    "produits": MilvusProduitCrud.MilvusProduitsCrud,
+    "fournisseurs": MilvusFournisseursCrud.MilvusFournisseursCrud,
+    "devis": MilvusDevisCrud.MilvusDevisCrud,
+    "websites": MilvusWebsiteCrud.MilvusWebsiteCrud,
+    "categories": MilvusCategorieCrud.MilvusCategorieCrud,
+    "echanges": MilvusEchangeCrud.MilvusEchangeCrud
+}
