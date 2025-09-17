db186e6b1933fceb6c4194fe8a68660d335b5892
Tanjona Ralita <tanjonaravelonapgmail.com>
2025-09-17 10:15:04 +0300

    feat(maj) : centraliser fonction

--- apps-microservices/api-rest-milvus/app/router/mapping_rest_milvus.py
+++ apps-microservices/api-rest-milvus/app/router/mapping_rest_milvus.py
@@ -1,20 +1,27 @@
-# common_utils/database/milvus_router_registry.py
-
-from common_utils.database import (
-    MilvusProduitCrud,
-    MilvusFournisseursCrud,
-    MilvusDevisCrud,
-    MilvusWebsiteCrud,
-    MilvusCategoriesCrud,
-    MilvusEchangeCrud
-)
-
 # Dictionnaire centralisé pour toutes les opérations CRUD
-MILVUS_CRUD_REGISTRY = {
-    "produits": MilvusProduitCrud.MilvusProduitsCrud,
-    "fournisseurs": MilvusFournisseursCrud.MilvusFournisseursCrud,
-    "devis": MilvusDevisCrud.MilvusDevisCrud,
-    "websites": MilvusWebsiteCrud.MilvusWebsiteCrud,
-    "categories": MilvusCategoriesCrud.MilvusCategoriesCrud,
-    "echanges": MilvusEchangeCrud.MilvusEchangeCrud
+MILVUS_COLLECTIONS = {
+    "produits": "produits_3",
+    "fournisseurs": "fournisseurs",
+    "devis": "devis",
+    "websites": "websites",
+    "categories": "categories",
+    "echanges": "echanges"
+}
+
+MILVUS_COLLECTIONS_DEFAULT_FIELDS = {
+    "produits": [
+        "id",
+        "id_produit",
+        "nom_produit",
+        "id_fournisseur",
+        "fournisseur",
+        "id_categorie",
+        "categorie",
+        "chunk_id"
+    ],
+    "fournisseurs": [],
+    "devis"       : [],
+    "websites"    : [],
+    "categories"  : [],
+    "echanges"    : [],
 }
