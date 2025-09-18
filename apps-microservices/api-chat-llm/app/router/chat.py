c1c5b860bfde7b0d73f3cb79e00c54a2d1f4c642
HosanaHt <haingatiana@hellopro.fr>
2025-09-18 14:27:43 +0300

    feat(new): chat llm

---
+++ apps-microservices/api-chat-llm/app/router/chat.py
@@ -0,0 +1,27 @@
+from fastapi import APIRouter, HTTPException, Body
+from app.schemas.chat import ChatRequest, chatResponse
+# from app.core.search import search_in_milvus
+from app.core.chat import get_chat_completion_response 
+
+import logging
+
+router = APIRouter()
+logger = logging.getLogger(__name__)
+    
+
+@router.post("/llm/chat", tags=["Chat - LLM"])
+async def chat_completion_llm(request: ChatRequest = Body(...)):
+    try:
+        logger.info(f"Requête chat completion sur llm : {request.prompt}")
+        if not request.prompt.strip():
+            raise ValueError("Le promt ne peut pas être vide.")        
+        
+        results = get_chat_completion_response(request)
+        logger.info(f"Résultats de la chat complesion: {results}")
+        return chatResponse(response=results["response"], chat_model=request.chat_model , temperature=request.temperature , time_elapsed=results.get("time_elapsed", None))
+    except ValueError as ve:
+        logger.error(f"Erreur de validation (400): {ve}")
+        raise HTTPException(status_code=400, detail=str(ve))
+    except Exception as e:
+        logger.error(f"Erreur interne du serveur (500): {e}", exc_info=True)
+        raise HTTPException(status_code=500, detail=f"Erreur interne du serveur: {e}")
---
+++ apps-microservices/api-chat-llm/main.py
@@ -0,0 +1,75 @@
+import time
+from fastapi import FastAPI
+from app.router import chat as llm_chat_router
+from app.core.credentials import settings
+
+import logging
+import asyncio
+from contextlib import asynccontextmanager
+
+# TODO:
+# centraliser la configuration du logging dans un module dédié
+# from app.core.searchws import get_milvus_connection, get_embedding_model, get_openai_client, prepare_onnx_model, get_reranker_onnx_model
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+# @asynccontextmanager
+# async def lifespan(app: FastAPI):
+#     """
+#     Handles application startup and shutdown events.
+#     """
+#     logger.info("--- Application Startup ---")
+    
+#     # Create a list of async tasks for pre-loading resources.
+#     # We run the synchronous, blocking model-loading code in a separate thread
+#     # to avoid blocking the asyncio event loop.
+#     init_tasks = [
+#         asyncio.to_thread(get_milvus_connection),
+#         asyncio.to_thread(get_embedding_model),
+#         # asyncio.to_thread(get_reranker_model),
+#         asyncio.to_thread(get_openai_client)
+#     ]
+    
+#     logger.info("Pre-loading models and establishing database connection...")
+#     # asyncio.gather runs all our initialization tasks concurrently
+#     await asyncio.gather(*init_tasks)
+    
+#     logger.info("--- Startup Complete. Application is ready. ---")
+    
+#     yield
+    
+#     # --- Shutdown Logic ---
+#     # You can add cleanup code here if needed, like closing connections.
+#     logger.info("--- Application Shutdown ---")
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    version=settings.PROJECT_VERSION,
+    description="API pour interroger Qdrant ou Milvus et générer des réponses avec des LLMs.",
+    # lifespan=lifespan
+)
+
+@app.on_event("startup")
+def startup():
+    """
+    Événement exécuté au démarrage de l'application.
+    - Étape 1: Vérifie si le modèle ONNX existe. Si non, le convertit.
+    - Étape 2: Pré-charge tous les modèles en mémoire pour une latence minimale.
+    """
+    logger.info("--- DÉMARRAGE DU SERVEUR : PRÉPARATION DES MODÈLES ---")
+    
+    # Étape 1: S'assure que le modèle ONNX existe sur le disque.
+    # Cette opération est longue UNIQUEMENT au tout premier lancement.
+    # prepare_onnx_model()
+    
+    logger.info("Pré-chargement du modèle de reranking ONNX...")
+    # get_reranker_onnx_model()
+    
+    logger.info("--- MODÈLES PRÊTS : LE SERVEUR EST OPÉRATIONNEL ---")
+
+app.include_router(llm_chat_router.router)
+
+@app.get("/", tags=["Monitoring"])
+def read_root():
+    return {"message": f"Bienvenue sur l'API {settings.PROJECT_NAME} v{settings.PROJECT_VERSION}"}
