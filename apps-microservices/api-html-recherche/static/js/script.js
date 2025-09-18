66606802094b110cb6ad160cbdca2a5fdba7a22f
sandrianirinaharivelo <sandrianirinaharivelo@hellopro.fr>
2025-09-18 12:38:51 +0300

    fix: modification url socket vers api-gateway

--- apps-microservices/api-html-recherche/static/js/script.js
+++ apps-microservices/api-html-recherche/static/js/script.js
@@ -1283,7 +1283,7 @@ $(function () {
     }
 
     // const wsUrl = "ws://34.90.162.9:8510/ws/search"; // L'URL est maintenant ici VM1
-    const wsUrl = "ws://34.34.166.5:8510/ws/search"; // L'URL est maintenant ici
+    const wsUrl = "ws://34.34.166.5:8500/search-service/ws/search"; // L'URL est maintenant ici
     console.log(`Connexion à ${wsUrl}...`);
 
     try {
