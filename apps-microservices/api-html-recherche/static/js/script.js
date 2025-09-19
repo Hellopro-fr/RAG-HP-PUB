753e366ded699a806911eaf215cee833b5eead46
sandrianirinaharivelo <sandrianirinaharivelo@hellopro.fr>
2025-09-19 17:48:33 +0300

    feat(maj): log reponse depuis l'api

--- apps-microservices/api-html-recherche/static/js/script.js
+++ apps-microservices/api-html-recherche/static/js/script.js
@@ -1797,6 +1797,7 @@ const connectWebSocket = () => {
         if (data.type === 'transcript' && data.transcript) {
             // Utilisation de .val() pour définir la valeur d'un input
             $searchInput.val(data.transcript);
+            console.log(data.transcript);
         } else if (data.type === 'error') {
             console.error('Server error:', data.error);
             // $errorDiv.text(`Erreur du serveur: ${data.error}`);
