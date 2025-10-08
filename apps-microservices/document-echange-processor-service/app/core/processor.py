64a0755b8908370428e34686834fd92932e86922
JeanPierre04 <stolojanahary@hellopro.fr>
2025-10-08 09:40:06 +0200

    Feat(maj): Suppression trace inutile

--- apps-microservices/document-echange-processor-service/app/core/processor.py
+++ apps-microservices/document-echange-processor-service/app/core/processor.py
@@ -120,14 +120,10 @@ async def process_document_data_for_templating(document_data: dict, bdd: str = "
         cleaner      = CleanHTML(texts)
         cleaned_text = cleaner.clean()
 
-        logger.info(f"\n\nTexte juste après nettoyage : {cleaned_text}")
-
         # Anonymisation
         anonymize = AnonymizeText()
         anonymized_text     = anonymize.anonymize_text(cleaned_text)
         text_to_embed_clean = anonymize.normalize_text(anonymized_text)
-
-        logger.info(f"\n\nTexte juste après Anonymisation : {text_to_embed_clean}")
 
         # Suppression des info inutiles via llm
         payload = {
