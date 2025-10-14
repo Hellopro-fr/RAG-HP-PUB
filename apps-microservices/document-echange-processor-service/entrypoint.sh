09972f45cd8d7ec802570ffc0c93d803d6fed5d3
JeanPierre04 <stolojanahary@hellopro.fr>
2025-10-14 10:36:33 +0200

    Feat(maj): modification lancement OCR service 1.0

--- apps-microservices/document-echange-processor-service/entrypoint.sh
+++ apps-microservices/document-echange-processor-service/entrypoint.sh
@@ -9,6 +9,9 @@ VLLM_PORT="8181"
 # Le port où docext.app.app lui-même écoute (si c'est un proxy ou une API frontale pour vLLM)
 DOCEXT_APP_PORT="8559"
 
+# Lancer ton application principale
+python -u -m document_echange_processor_service.main &
+
 python -m docext.app.app \
   --model_name hosted_vllm/nanonets/Nanonets-OCR-s \
   --gpu_memory_utilization 0.95 \
@@ -16,7 +19,5 @@ python -m docext.app.app \
   --max_num_imgs 2 \
   --dtype float16 \
   --server_port "${DOCEXT_APP_PORT}" \
-  --vlm_server_port "${VLLM_PORT}" &
+  --vlm_server_port "${VLLM_PORT}"
 
-# Lancer ton application principale
-python -u -m document_echange_processor_service.main
