cd7a3438da357151f02f0967a1bea00ac2a4fd04
sandrianirinaharivelo <sandrianirinaharivelo@hellopro.fr>
2025-10-21 12:02:58 +0300

    feat(maj): modification return api llm avec mode thinking

--- apps-microservices/llm-service/infrastructure/vllm_client.py
+++ apps-microservices/llm-service/infrastructure/vllm_client.py
@@ -84,7 +84,8 @@ class VLLMClient:
                     response.raise_for_status()
                     response_data = response.json()
                     if 'choices' in response_data and len(response_data['choices']) > 0:
-                        return re.sub(r'<think>.*?</think>', '', response_data['choices'][0].get('message', {}).get('content', ''), flags=re.DOTALL)
+                        # return re.sub(r'<think>.*?</think>', '', response_data['choices'][0].get('message', {}).get('content', ''), flags=re.DOTALL)
+                        return re.sub(r'\s+', ' ', re.sub(r'<think>.*?</think>', '', response_data['choices'][0].get('message', {}).get('content', ''), flags=re.DOTALL)).strip()
                     return "[ERREUR: Réponse inattendue du service LLM]"
             except httpx.TimeoutException:
                 logging.error(f"Timeout dépassé lors de la requête non-streamée vers vLLM.")
