7b8d3516944ec2e5650c99b03f463da2e9399a32
sandrianirinaharivelo <sandrianirinaharivelo@hellopro.fr>
2025-10-22 12:20:16 +0300

    feat(maj): ajout retour options dans retour chat llm

--- libs/common-utils/src/common_utils/grpc_clients/schemas/chat.py
+++ libs/common-utils/src/common_utils/grpc_clients/schemas/chat.py
@@ -1,9 +1,14 @@
 from typing import Any, Dict, Optional
 from pydantic import BaseModel, Field
 
+
 class ChatRequest(BaseModel):
     prompt: str
     temperature: float = 0.7
     max_tokens: int = 1024
     enable_thinking: bool = False
-    options: Optional[Dict[str, Any]] = {}
+    options: Optional[Dict[str, Any]] = {
+        "top_p": float(0.8),
+        "top_k": int(20),
+        "repetition_penalty": float(1.0),
+    }
