f136060d871442356aeeb2f3166616e6f43f0410
sandrianirinaharivelo <sandrianirinaharivelo@hellopro.fr>
2025-10-24 12:31:25 +0300

    fix: appel api deepseek

--- libs/common-utils/src/common_utils/grpc_clients/schemas/chat.py
+++ libs/common-utils/src/common_utils/grpc_clients/schemas/chat.py
@@ -10,5 +10,5 @@ class ChatRequest(BaseModel):
     options: Optional[Dict[str, Any]] = {
         "top_p": float(0.8),
         "top_k": int(20),
-        "repetition_penalty": float(1.0),
+        # "repetition_penalty": float(1.0),
     }
