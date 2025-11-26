aa4c4a76d071db4d488275b7bd3df123c23f08ce
JeanPierre04 <stolojanahary@hellopro.fr>
2025-11-26 12:14:26 +0100

    Feat(maj): debug nettoyage-bruit-ocr

--- apps-microservices/nettoyage-bruit-ocr-service/app/core/processor.py
+++ apps-microservices/nettoyage-bruit-ocr-service/app/core/processor.py
@@ -116,7 +116,13 @@ async def _process_single_message(document_item: dict) -> dict:
             elif contenu != "ok":
                 cleaned_text = contenu
         else:
-            raise ValueError(f"Aucun bloc JSON trouvé dans la sortie du LLM: {raw_text}")
+            # raise ValueError(f"Aucun bloc JSON trouvé dans la sortie du LLM")
+            return {
+                "status": "error",
+                "original_message": document_item,
+                "error_message": "Aucun bloc JSON trouvé dans la sortie du LLM",
+                "metric_payload": metric_payload
+            }
 
     except Exception as e:
         logging.warning(f"Erreur lors du nettoyage LLM : {type(e).__name__} - {e}")
