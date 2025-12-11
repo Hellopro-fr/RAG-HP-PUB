b53027b79d11cf8c25ee901897a5ead6ca6c7bf1
JeanPierre04 <stolojanahary@hellopro.fr>
2025-12-11 12:42:55 +0100

    Feat(maj): gestion param options request milvus_client 1.4

--- apps-microservices/database-recherche-service/infrastructure/grpc_server.py
+++ apps-microservices/database-recherche-service/infrastructure/grpc_server.py
@@ -18,18 +18,18 @@ class DatabaseSearchServiceImpl(database_pb2_grpc.DatabaseSearchServiceServicer)
         logging.info(f"Requête de recherche reçue pour la collection '{request.collection_name}' avec top_k={request.top_k}")
         # TODO: Sécuriser ce flux (authentification, validation des entrées)
         try:
-            kwargs = {}
-            if "options" in request:
-                kwargs = MessageToDict(request['options'])
+            # kwargs = {}
+            # if "options" in request:
+            #     kwargs = MessageToDict(request['options'])
 
-            logging.info(f"Request options : {kwargs}")
+            # logging.info(f"Request options : {kwargs}")
             results = self.use_case.execute_search(
                 collection_name=request.collection_name,
                 vector=list(request.query_embedding),
                 top_k=request.top_k,
                 filter_expression=request.filter_expression if request.HasField('filter_expression') else None,
                 output_fields=list(request.output_fields) if request.output_fields else None,
-                **kwargs
+                # **kwargs
             )
             
             proto_results = []
