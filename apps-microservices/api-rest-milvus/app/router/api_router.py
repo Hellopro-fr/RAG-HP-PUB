e37a99677b5265db1e2baf3c237270347dc8bc1b
fsolondraza <fsolondraza@hellopro.fr>
2026-01-07 16:15:53 +0300

    feat(check-url-api-rest-milvus): Mise en place de l'endpoint check-urls-existence

--- apps-microservices/api-rest-milvus/app/router/api_router.py
+++ apps-microservices/api-rest-milvus/app/router/api_router.py
@@ -5,6 +5,7 @@ from .read import router as read_router
 from .read_post import router as read_post_router
 from .update import router as update_router
 from .delete import router as delete_router
+from .check_urls import router as check_urls_router
 
 api_router = APIRouter()
 
@@ -13,3 +14,4 @@ api_router.include_router(read_post_router, tags=["POST Search"])
 api_router.include_router(create_router, tags=["POST"])
 api_router.include_router(update_router, tags=["PUT"])
 api_router.include_router(delete_router, tags=["DELETE"])  # Désactivé pour sécurité
+api_router.include_router(check_urls_router, tags=["URL Verification"])
