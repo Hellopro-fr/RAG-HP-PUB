d2ad88ae805221a95a70df4cab93c1a05a386104
Rindra ANDRIANJANAKA <randrianjanaka@hellopro.fr>
2026-01-20 15:15:22 +0300

    feat: Correcting URL for Deepseek-OCR

--- libs/common-utils/src/common_utils/ocr/DeepseekOCRDocExtractor.py
+++ libs/common-utils/src/common_utils/ocr/DeepseekOCRDocExtractor.py
@@ -11,7 +11,7 @@ import os
 from pypdf import PdfReader
 
 
-BASE_URL_OCR = os.environ.get("URL_OCR", "http://deepseek-ocr:8501")
+BASE_URL_OCR = os.environ.get("URL_OCR", "https://api.hellopro.eu/deepseek_ocr-service")
 MAX_PDF_PAGES = int(os.environ.get("MAX_PDF_PAGES", "19"))
 
 
