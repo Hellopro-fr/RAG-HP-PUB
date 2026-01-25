c7a2351c2c127283b9036923b0407da49ab5e1b5
Rindra ANDRIANJANAKA <randrianjanaka@hellopro.fr>
2026-01-25 21:51:56 +0300

    feat: Crawler-Service-Python : Fix error

--- apps-microservices/crawler-service-python/src/utils.py
+++ apps-microservices/crawler-service-python/src/utils.py
@@ -8,7 +8,7 @@ from playwright.async_api import Page
 import logging
 import json
 from urllib.parse import urlparse, parse_qs, urlencode, urlunparse
-from typing import Optional
+from typing import Optional, Union
 
 # Imports for Reclaim Logic
 from crawlee.storages import Dataset, RequestQueue
