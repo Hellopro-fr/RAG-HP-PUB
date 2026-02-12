bc4440e36b0146819b790dede7dae134b2ee9767
sandrianirinaharivelo <sandrianirinaharivelo@hellopro.fr>
2026-02-12 16:01:54 +0300

    feat(maj): modification score zone geo à 1

--- apps-microservices/graph-rag-api-recherche-service/app/services/recommendation_service.py
+++ apps-microservices/graph-rag-api-recherche-service/app/services/recommendation_service.py
@@ -1129,7 +1129,9 @@ class RecommendationService:
         // Calculate final_score = global_score * zone_score * etat_score * typo_score
         // Filter out products with negative final_score
         WITH p, details, global_score, zone_score, etat_score, typo_score, info_soc,
-             global_score * zone_score * etat_score * typo_score AS final_score
+             // forcer zone_geo à 1
+             // global_score * zone_score * etat_score * typo_score AS final_score
+             global_score * 1 * etat_score * typo_score AS final_score
         WHERE final_score >= 0
         WITH p, details, global_score, zone_score, etat_score, typo_score, final_score, info_soc
         ORDER BY final_score DESC
