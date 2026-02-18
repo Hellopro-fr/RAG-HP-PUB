67657c8124ed1337710319a6202f6de36d3f3c13
sandrianirinaharivelo <sandrianirinaharivelo@hellopro.fr>
2026-02-18 16:04:05 +0300

    feat(maj): ajout debug info produit

--- apps-microservices/graph-rag-api-recherche-service/app/services/recommendation_service.py
+++ apps-microservices/graph-rag-api-recherche-service/app/services/recommendation_service.py
@@ -593,6 +593,10 @@ class RecommendationService:
         max_per_supplier_extended = request.scoring.max_per_supplier_extended
         score_step = request.scoring.score_step
 
+        logging.warning(f"[DEBUG] scoring parameters: {request.scoring}")
+        logging.warning(
+            f"[DEBUG] scoring parameters assigné: {blocked_val}, {different_val}, {z_unmatched}, {e_unmatched}, {g_unknown_score}, {c_unknown_score}, {t_unmatched}"
+        )
         # Extract user location data from metadonnee_utilisateurs
         user_meta = request.metadonnee_utilisateurs
         user_cp = user_meta.cp if user_meta else None
