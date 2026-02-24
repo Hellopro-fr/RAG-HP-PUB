d0ce2ba2a8f2f6db8aae6e32e85aa4f5830ce286
JeanPierre04 <stolojanahary@hellopro.fr>
2026-02-24 10:07:30 +0100

    Feat(maj): api-detection-langue-fr ajout trace contenu de l'url1.4

--- apps-microservices/api-detection-langue-fr/app/services/redirect_tracker.py
+++ apps-microservices/api-detection-langue-fr/app/services/redirect_tracker.py
@@ -12,7 +12,7 @@ BROWSER_HEADERS = {
     'User-Agent': settings.USER_AGENT,
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
     'Accept-Language': 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7',
-    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Encoding': 'gzip, deflate',
     'Cache-Control': 'no-cache',
     'Pragma': 'no-cache',
     'Sec-Ch-Ua': '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
