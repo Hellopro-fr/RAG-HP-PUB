2a5041ee92fc8e7743347342b499fd110dd2ce7a
fsolondraza <fsolondraza@hellopro.fr>
2026-04-24 18:13:23 +0300

    feat(coherence): rule peak_ram_exceeds_allocated + pastille in CapacityPlanning

--- apps-microservices/crawler-monitor-frontend/src/coherence/rules/index.js
+++ apps-microservices/crawler-monitor-frontend/src/coherence/rules/index.js
@@ -1,5 +1,6 @@
 import replicasVsMaxSlots from './replicas_vs_max_slots';
 import replicaJobMapping from './replica_job_mapping';
+import peakRamExceedsAllocated from './peak_ram_exceeds_allocated';
 
 /** @type {import('../types').Rule[]} */
-export const RULES = [replicasVsMaxSlots, replicaJobMapping];
+export const RULES = [replicasVsMaxSlots, replicaJobMapping, peakRamExceedsAllocated];
