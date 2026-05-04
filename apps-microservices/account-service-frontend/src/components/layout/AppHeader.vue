ac037c749b7ce33d3dfe8326628471d966ce4c03
sandrianirinaharivelo <sandrianirinaharivelo@hellopro.fr>
2026-05-04 16:53:24 +0300

    feat(account-service-frontend): drop global search bar from header
    The header search served the TailAdmin demo "jump to anything" UX
    that does not exist here. Per-page DataTable search covers the actual
    need.
    
    feat(account-service-frontend) : suppression de la barre de recherche du header

--- apps-microservices/account-service-frontend/src/components/layout/AppHeader.vue
+++ apps-microservices/account-service-frontend/src/components/layout/AppHeader.vue
@@ -67,7 +67,6 @@
             />
           </svg>
         </button>
-        <SearchBar />
       </div>
 
       <div
@@ -87,7 +86,6 @@
 import { ref } from 'vue'
 import { useSidebar } from '@/composables/useSidebar'
 import ThemeToggler from '../common/ThemeToggler.vue'
-import SearchBar from './header/SearchBar.vue'
 import HeaderLogo from './header/HeaderLogo.vue'
 import UserMenu from './header/UserMenu.vue'
 
